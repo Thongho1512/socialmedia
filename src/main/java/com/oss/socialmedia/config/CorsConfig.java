2a4d1541c28782b7abd3b5d90fcf8a61923b9696
= <=>
2025-02-14 17:48:09 +0700

    configure cors

--- src/main/java/com/oss/socialmedia/config/CorsConfig.java
+++ src/main/java/com/oss/socialmedia/config/CorsConfig.java
@@ -1,23 +1,29 @@
 package com.oss.socialmedia.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    @Bean 
+    public CorsConfigurationSource corsConfigurationSource() { 
+    CorsConfiguration configuration = new CorsConfiguration(); 
+        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500"));         
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", 
+        "OPTIONS")); // Allowed methods 
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", 
+        "Accept")); 
+        configuration.setAllowCredentials(true); 
+        configuration.setMaxAge(3600L); 
+        // How long the response from a pre-flight request can be cached by clients 
+        UrlBasedCorsConfigurationSource source = new 
+        UrlBasedCorsConfigurationSource(); 
+        source.registerCorsConfiguration("/**", configuration); // Apply this configuration to all paths 
+    return source; 
     }
 }
